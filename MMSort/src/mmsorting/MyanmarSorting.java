bb0a0e93ca7f9fcc99dace391ebf599fdb3d69ab
KhinThandarNwet <thandar60@gmail.com>
2023-02-14 21:29:48 +0630

    Update MyanmarSorting.java

--- MMSort/src/mmsorting/MyanmarSorting.java
+++ MMSort/src/mmsorting/MyanmarSorting.java
@@ -6,16 +6,13 @@ import java.util.List;
 import java.util.Locale;
 
 import com.ibm.icu.text.Collator;
-
-/**
-
- */
+//KTDN
 public class MyanmarSorting {
 
 	public static void main(String[] args) {
 
-		//List<String> myanmarWordList = Arrays.asList("ရှမ်း", "ချင်း", "ကချင်", "ကျွန်တော်", "ကရင်", "ရခိုင်", "ဗမာ","ကာ","က",  "ကယား",  "မွန်");
-		List<String> myanmarWordList = Arrays.asList("ကူးသန်း", "ကစားပွဲ", "ကိုကင်း", "ကျွန်တော်", "ကိုင်းပျ", "ကီလိုမီတာ", "ဗမာ","ကာ","က",  "ကယား",  "မွန်");
+		
+		List<String> myanmarWordList = Arrays.asList("ကူးသန်း", "ကစားပွဲ", "ကိုကင်း", "ကျွန်တော်", "ကိုင်းပျ", "ကီလိုမီတာ", "ဗမာ","ကား","က",  "ကယား",  "ကမ္ဘာကျော်");
 		Locale myanmar = new Locale("my", "MM", "");
 
 		Collator coll = Collator.getInstance(myanmar);
